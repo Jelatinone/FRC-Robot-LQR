87e26d21fdd2841a30ce2b99879cd9aa661f01be
Cody W. <CodyW2125@outlook.com>
2023-09-19 00:29:24 -0500

    Update Constants

--- src/main/java/frc/robot/subsystems/drivebase/Constants.java
+++ src/main/java/frc/robot/subsystems/drivebase/Constants.java
@@ -26,21 +26,20 @@ public final class Constants {
     
 
   public static final class Values {
-    public static final Integer PATHPLANNER_SERVER_PORT = (6969); 
-    public static final Double DRIVETRAIN_GEAR_RATIO = (6.75);    
     public static final Double WHEEL_DIAMETER = (0.1016);    
     public static final Double WHEEL_PERIMETER = (WHEEL_DIAMETER) * Math.PI;
     public static final Double BUILTIN_ENCODER_SENSITIVITY = (2048.0);
-    public static final Double ROBOT_WIDTH = (0.6858);    
+    public static final Double MAXIMUM_VELOCITY = (15.0);    
     public static final Double SCALE_FACTOR = (1.0/50.0);
-
+    public static final Double ROBOT_WIDTH = (0.6858);    
+    public static final Double DRIVETRAIN_GEAR_RATIO = (6.75);    
+    public static final Integer PATHPLANNER_SERVER_PORT = (6969);     
     public static final Integer GYROSCOPE_ID = (3);
 
   }
 
   public static final class Hardware {
     public static final WPI_Pigeon2 GYROSCOPE = new WPI_Pigeon2(Values.GYROSCOPE_ID);
-
 
     public static final class Modules {
         public static final class FL_Module {
@@ -103,8 +102,8 @@ public final class Constants {
             public static final Double MAXIMUM_AZIMUTH_VOLTAGE = (12.0);            
             public static final Double MAXIMM_AZIMUTH_VELOCITY = (5.4);              
             public static final Double MAXIMUM_LINEAR_VELOCITY = (5.4);
-            public static final Double ACCELERATION_GAIN = (1.0); //<---- Acceleration Gain (kA) of Linear Position System, Volts/(Units/Sec^2)
-            public static final Double VELOCITY_GAIN = (1.0); //<---- Velocity Gain (kV) of Linear Position System, Volts/(Units/Sec)
+            public static final Double ACCELERATION_GAIN = (1.0);
+            public static final Double VELOCITY_GAIN = (1.0);
 
             public static final Double ENCODER_POSITION_ACCURACY_DEGREES = (1/4096.0); 
             public static final Double MODEL_VELOCITY_ACCURACY_DEGREES = (7e-5);
@@ -134,15 +133,15 @@ public final class Constants {
               PLANT,
               new LinearQuadraticRegulator<>(
                 PLANT,
-                VecBuilder.fill(Units.degreesToRadians(Components.AZIMUTH_SENSOR.getAbsolutePosition()),Units.rotationsPerMinuteToRadiansPerSecond(Components.AZIMUTH_SENSOR.getVelocity())), //<---- Controller error weight, how much we want to conserve controller error, or how accurate we want to be
-                VecBuilder.fill(Values.MAXIMUM_AZIMUTH_VOLTAGE), //<---- Controller effort weight, how much we want to conserve controller expenditure or how fast we want to be; increased state error
+                VecBuilder.fill(Units.degreesToRadians(Components.AZIMUTH_SENSOR.getAbsolutePosition()),Units.rotationsPerMinuteToRadiansPerSecond(Components.AZIMUTH_SENSOR.getVelocity())),
+                VecBuilder.fill(Values.MAXIMUM_AZIMUTH_VOLTAGE),
                 Values.DISCRETIZATION_TIMESTEP),
               new KalmanFilter<>(
                 Nat.N2(),
                 Nat.N1(),
                 PLANT, 
-                VecBuilder.fill(Units.degreesToRadians(Values.MODEL_VELOCITY_ACCURACY_DEGREES),Units.rotationsPerMinuteToRadiansPerSecond(Values.MODEL_POSITION_ACCURACY_RPM)), //<---- How accurate we think our models are
-                VecBuilder.fill(Units.rotationsPerMinuteToRadiansPerSecond(Values.ENCODER_POSITION_ACCURACY_DEGREES)), //<---- How accurate we think our encoders are
+                VecBuilder.fill(Units.degreesToRadians(Values.MODEL_VELOCITY_ACCURACY_DEGREES),Units.rotationsPerMinuteToRadiansPerSecond(Values.MODEL_POSITION_ACCURACY_RPM)), 
+                VecBuilder.fill(Units.rotationsPerMinuteToRadiansPerSecond(Values.ENCODER_POSITION_ACCURACY_DEGREES)),
                 Values.DISCRETIZATION_TIMESTEP),
               Values.MAXIMUM_AZIMUTH_VOLTAGE,
               Values.DISCRETIZATION_TIMESTEP);             
@@ -157,8 +156,8 @@ public final class Constants {
             public static final Double MAXIMUM_AZIMUTH_VOLTAGE = (12.0);            
             public static final Double MAXIMM_AZIMUTH_VELOCITY = (5.4);              
             public static final Double MAXIMUM_LINEAR_VELOCITY = (5.4);
-            public static final Double ACCELERATION_GAIN = (1.0); //<---- Acceleration Gain (kA) of Linear Position System, Volts/(Units/Sec^2)
-            public static final Double VELOCITY_GAIN = (1.0); //<---- Velocity Gain (kV) of Linear Position System, Volts/(Units/Sec)
+            public static final Double ACCELERATION_GAIN = (1.0);
+            public static final Double VELOCITY_GAIN = (1.0); 
 
             public static final Double ENCODER_POSITION_ACCURACY_DEGREES = (1/4096.0); 
             public static final Double MODEL_VELOCITY_ACCURACY_DEGREES = (7e-5);
@@ -188,15 +187,15 @@ public final class Constants {
               PLANT,
               new LinearQuadraticRegulator<>(
                 PLANT,
-                VecBuilder.fill(Units.degreesToRadians(Components.AZIMUTH_SENSOR.getAbsolutePosition()),Units.rotationsPerMinuteToRadiansPerSecond(Components.AZIMUTH_SENSOR.getVelocity())), //<---- Controller error weight, how much we want to conserve controller error, or how accurate we want to be
-                VecBuilder.fill(Values.MAXIMUM_AZIMUTH_VOLTAGE), //<---- Controller effort weight, how much we want to conserve controller expenditure or how fast we want to be; increased state error
+                VecBuilder.fill(Units.degreesToRadians(Components.AZIMUTH_SENSOR.getAbsolutePosition()),Units.rotationsPerMinuteToRadiansPerSecond(Components.AZIMUTH_SENSOR.getVelocity())),
+                VecBuilder.fill(Values.MAXIMUM_AZIMUTH_VOLTAGE), 
                 Values.DISCRETIZATION_TIMESTEP),
               new KalmanFilter<>(
                 Nat.N2(),
                 Nat.N1(),
                 PLANT, 
-                VecBuilder.fill(Units.degreesToRadians(Values.MODEL_VELOCITY_ACCURACY_DEGREES),Units.rotationsPerMinuteToRadiansPerSecond(Values.MODEL_POSITION_ACCURACY_RPM)), //<---- How accurate we think our models are
-                VecBuilder.fill(Units.rotationsPerMinuteToRadiansPerSecond(Values.ENCODER_POSITION_ACCURACY_DEGREES)), //<---- How accurate we think our encoders are
+                VecBuilder.fill(Units.degreesToRadians(Values.MODEL_VELOCITY_ACCURACY_DEGREES),Units.rotationsPerMinuteToRadiansPerSecond(Values.MODEL_POSITION_ACCURACY_RPM)), 
+                VecBuilder.fill(Units.rotationsPerMinuteToRadiansPerSecond(Values.ENCODER_POSITION_ACCURACY_DEGREES)), 
                 Values.DISCRETIZATION_TIMESTEP),
               Values.MAXIMUM_AZIMUTH_VOLTAGE,
               Values.DISCRETIZATION_TIMESTEP);          
@@ -210,8 +209,8 @@ public final class Constants {
             public static final Double MAXIMUM_AZIMUTH_VOLTAGE = (12.0);            
             public static final Double MAXIMM_AZIMUTH_VELOCITY = (5.4);              
             public static final Double MAXIMUM_LINEAR_VELOCITY = (5.4);
-            public static final Double ACCELERATION_GAIN = (1.0); //<---- Acceleration Gain (kA) of Linear Position System, Volts/(Units/Sec^2)
-            public static final Double VELOCITY_GAIN = (1.0); //<---- Velocity Gain (kV) of Linear Position System, Volts/(Units/Sec)
+            public static final Double ACCELERATION_GAIN = (1.0);
+            public static final Double VELOCITY_GAIN = (1.0);
 
             public static final Double ENCODER_POSITION_ACCURACY_DEGREES = (1/4096.0); 
             public static final Double MODEL_VELOCITY_ACCURACY_DEGREES = (7e-5);
@@ -241,15 +240,15 @@ public final class Constants {
               PLANT,
               new LinearQuadraticRegulator<>(
                 PLANT,
-                VecBuilder.fill(Units.degreesToRadians(Components.AZIMUTH_SENSOR.getAbsolutePosition()),Units.rotationsPerMinuteToRadiansPerSecond(Components.AZIMUTH_SENSOR.getVelocity())), //<---- Controller error weight, how much we want to conserve controller error, or how accurate we want to be
-                VecBuilder.fill(Values.MAXIMUM_AZIMUTH_VOLTAGE), //<---- Controller effort weight, how much we want to conserve controller expenditure or how fast we want to be; increased state error
+                VecBuilder.fill(Units.degreesToRadians(Components.AZIMUTH_SENSOR.getAbsolutePosition()),Units.rotationsPerMinuteToRadiansPerSecond(Components.AZIMUTH_SENSOR.getVelocity())), 
+                VecBuilder.fill(Values.MAXIMUM_AZIMUTH_VOLTAGE),
                 Values.DISCRETIZATION_TIMESTEP),
               new KalmanFilter<>(
                 Nat.N2(),
                 Nat.N1(),
                 PLANT, 
-                VecBuilder.fill(Units.degreesToRadians(Values.MODEL_VELOCITY_ACCURACY_DEGREES),Units.rotationsPerMinuteToRadiansPerSecond(Values.MODEL_POSITION_ACCURACY_RPM)), //<---- How accurate we think our models are
-                VecBuilder.fill(Units.rotationsPerMinuteToRadiansPerSecond(Values.ENCODER_POSITION_ACCURACY_DEGREES)), //<---- How accurate we think our encoders are
+                VecBuilder.fill(Units.degreesToRadians(Values.MODEL_VELOCITY_ACCURACY_DEGREES),Units.rotationsPerMinuteToRadiansPerSecond(Values.MODEL_POSITION_ACCURACY_RPM)), 
+                VecBuilder.fill(Units.rotationsPerMinuteToRadiansPerSecond(Values.ENCODER_POSITION_ACCURACY_DEGREES)),
                 Values.DISCRETIZATION_TIMESTEP),
               Values.MAXIMUM_AZIMUTH_VOLTAGE,
               Values.DISCRETIZATION_TIMESTEP);        
